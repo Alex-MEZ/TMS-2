1aaeaa0cae8c46163faef4653b5defb2908516da
alex01 <e726983@gmail.com>
2023-03-01 14:57:32 +0300

    nothing

---
+++ my_sqlalchemy/sqlalchemy_02.py
@@ -0,0 +1,19 @@
+# Создать файл sqlalchemy_02.py.
+# Создать соединение к базе sa_test.db.
+# Создать 5 книг с помощью sqlalchemy.
+
+from sqlalchemy import create_engine, text
+
+engine = create_engine('sqlite3:///sa_test.db')
+with engine.connect() as connection:
+    insert_query = """
+                INSERT INTO Book (title, pages, author, price, release_year)
+            values ('Kok', 25,'Bul','1430',2300),
+            ('Kak', 225,'Bal','12430',230),
+            ('Kosk', 2225,'Ful','430',800),
+            ('Kdok', 4325,'Badl','143',6300),
+            ('Kfok', 2525,'Basl','13430',3300);
+            """
+    connection.commit()
+    query = text(insert_query)
+    connection.execute(query)
---
+++ my_sqlalchemy/sqlalchemy_03.py
@@ -0,0 +1,25 @@
+# Создать файл sqlalchemy_03.py.
+# Написать программу: пользователь вводит год.
+# Отобразить на экране те книги, год которых меньше введенного пользователем года.
+# Если таких книг нет - вывести сообщение: Not found.
+
+from sqlalchemy import create_engine, text
+
+
+def find_books_by_year():
+    engine = create_engine('sqlite3:///sa_test.db')
+    with engine.connect() as connection:
+        year = int(input('enter year - '))
+        select_query_text = f""" SELECT *FROM Book WHERE release_year < {year}"""
+        query = text(select_query_text)
+        data = list(connection.execute(query))
+        if len(data):
+            for book in data:
+                print(book)
+            else:
+                print("not found")
+
+def main():
+    find_books_by_year()
+
+if __name__=__main__
