48f5ff6b63316dbd39e6e2ef6b3fda63284c14a7
alex01 <e726983@gmail.com>
2023-03-06 21:59:14 +0300

    loops_01

---
+++ loops/loops_01.py
@@ -0,0 +1,9 @@
+# Выведите на экран n раз фразу "I am a programmer".
+# Число n вводит пользователь.
+
+n_raz = input("Сколько раз хочешь вывести фразу на экран? - ")
+
+my_list = (f'I am a programmer')
+for my_list in range(n_raz)
+    print(my_list)
+
