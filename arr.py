7b266c86fd9ef22a1e65f853017a3cbb16af6b23
alex01 <e726983@gmail.com>
2023-03-09 11:48:56 +0300

    arr

--- arr.py
+++ arr.py
@@ -8,11 +8,38 @@
 # 7*. Найти верхнюю и нижнюю квартиль
 import statistics
 
+import numpy
+
+# 1
 arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
 print(sum(arr))
 
-avg_arr = sum(arr)/len(arr)
+# 2
+avg_arr = sum(arr) / len(arr)
 print(avg_arr)
 
+# 3
+from scipy.stats import gmean
 
+avg2_arr = arr
+avg2_arr = gmean(avg2_arr)
+print(avg2_arr)
+
+# 4
+print(sum(x ** 2 for x in arr))
+
+# 5
+from numpy import cumsum
+
+massiv = numpy.cumsum(arr)
+print(massiv[-1], massiv)
+# or
+# print(sum(arr))
+
+
+#6
+import statistics
+print(statistics.median(arr))
+
+#7 ???
 
