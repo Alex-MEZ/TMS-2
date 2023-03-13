27c843be6219cbcf9c59354ab3ed75ea31bb0a10
alex01 <e726983@gmail.com>
2023-03-13 15:35:11 +0300

    loops_04

---
+++ loops/loops_04.py
@@ -0,0 +1,13 @@
+# Просуммировать неопределенное количество чисел, вводимых пользователем,
+# суммировать до тех пор, пока пользователь не введёт слово «стоп». Не учитывать числа кратные 5
+
+sum = 0
+while True:
+    num = input("Vvedi chislo (ili 'стоп' dlya zaversheniya: ")
+    if num == "стоп":
+        break
+    num_1 = int(num)
+    if num_1 % 5 == 0:
+        continue
+    sum += num_1
+print(sum)
