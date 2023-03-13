52945a432c7fc11330bc05c9766b45844ae6e9df
alex01 <e726983@gmail.com>
2023-03-13 15:21:41 +0300

    loops_03

--- loops/loops_03.py
+++ loops/loops_03.py
@@ -1,13 +1,21 @@
 # Просуммировать неопределенное количество чисел, вводимых пользователем,
 # суммировать до тех пор, пока пользователь не введёт слово «стоп»
 
-arr = input("Vvedi chislo(pishi stop kogda zahochesh ostanovitsya): ")
-i = int(arr)
-print(arr.isdigit())
-while arr.isdigit() == True:
-    i += i
-    print(i)
-    break
+sum =0
+while True:
+    num = input("Vvedi chislo (ili 'стоп' dlya zaversheniya: ")
+    if num == "стоп":
+        break
+    sum += float(num)
+
+print(sum)
+# arr = input("Vvedi chislo(pishi stop kogda zahochesh ostanovitsya): ")
+# i = int(arr)
+# print(arr.isdigit())
+# while arr.isdigit() == True:
+#     i += i
+#     print(i)
+#     break
 
 # while i in arr != 'stop':
 #     i += i
