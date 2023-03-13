d758d178c7f057bf226836bb3b297236159fa109
alex01 <e726983@gmail.com>
2023-03-13 19:25:39 +0300

    sort

---
+++ sort.py
@@ -0,0 +1,11 @@
+arr = [1, 7, 3, 4, 9, 6, 5, 3, 2]
+
+# for i in range(len(arr)):
+#     for j in range(i,len(arr)):
+#         if arr[i] > arr[j]:
+#             arr[i], arr[j] = arr[j], arr[i]
+# print(arr)
+
+
+
+while i in range(len(arr)):
