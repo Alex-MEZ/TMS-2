4b36ebd83d1e088a812e6c5df92c4746cc6ee4d1
alex01 <e726983@gmail.com>
2023-03-20 22:02:51 +0300

    _

---
+++ functions/funcs_02.py
@@ -0,0 +1,22 @@
+# Написать программу для работы с матрицами:
+# Создание
+# Вывод
+# Сумма всех элементов
+# Нахождение максимального элемента
+# Нахождение минимального элемента.
+
+from random import randint
+
+matrix = [[randint(1, 10) for _ in range(4)] for _ in range(4)]  # Создание матриц (диапазон, колличество)
+
+
+def my_func("create", 'vyvod', 'summa', 'max_number', 'min_nimber'):
+    print()
+
+
+def main():
+    my_func(name)
+
+
+if __name__ == '__main__':
+    main()
