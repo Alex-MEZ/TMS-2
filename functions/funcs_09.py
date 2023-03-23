4da8e9ee99593c82f954383fdeb1a1c99e48169b
alex01 <e726983@gmail.com>
2023-03-23 21:29:46 +0300

    funcs_09

--- functions/funcs_09.py
+++ functions/funcs_09.py
@@ -1,15 +1,29 @@
 # Дан список чисел. Посчитать сколько раз встречается каждое число. Использовать функцию.
+from random import randint
 
 
-def schet(*args):
-    for i in args:
+def schet(arr: list) -> dict:
+    dict = {}
+    for i in set(arr):
+        dict[i] = arr.count(i)
+    return dict
 
 
-
-
+def schet_2(arr: list) -> dict:
+    dict = {}
+    for i in arr:
+        if dict.get(i):
+            dict[i] = dict[i] + 1
+        else:
+            dict[i] = 1
+    return dict
 
 def main():
-    print(schet(1, 2, 3, 3, 2, 5, 3, 1, 3, 5, 3, 4, 6, 2))
+    arr = [randint(0, 9) for _ in range(15)]
+    print(arr)
+    print(schet(arr))
+    print(schet_2(arr))
+
 
 
 if __name__ == '__main__':
