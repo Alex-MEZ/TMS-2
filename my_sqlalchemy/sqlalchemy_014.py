3cb559c007112e96c46d2fe391217391fd931123
alex01 <e726983@gmail.com>
2023-06-05 21:10:22 +0300

    _

---
+++ my_sqlalchemy/sqlalchemy_014.py
@@ -0,0 +1,11 @@
+from school.models import session, Group, Student
+
+data = session.query(Group).join(Student, Group.id == Student.group_id).filter(Student.firstname == 'max').all()
+print(data)
+for i in data:
+    print(i.name)
+
+result2 = session.query(Student, Diary).join(Diary, Student.id == Diary.student_id).filter(Diary.avg_score > 6)
+print(list(result2))
+for st, d in result2:
+    print(st.firstname, d.avg_score, st.diary)
